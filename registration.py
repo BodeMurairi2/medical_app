64b1b098448a5104b538d0d542dc0473e2363258
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-19 15:16:22 +0200

    First commit Authentication class

--- registration.py
+++ registration.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # This script handles doctors data in our medical files database
-import os
-
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -21,22 +19,21 @@ Base = declarative_base()
 
 class Doctor(Base):
     __tablename__ = "Doctors"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key= True, unique=True, autoincrement=True)
     FIRST_NAME = Column(String)
     LAST_NAME = Column(String)
     SPECIALIZATION = Column(String)
-    LICENSE = Column(String)
+    LICENSE = Column(String, unique= True)
     DATE_OF_BIRTH = Column(String)
     SEX = Column(String)
-    EMAIL = Column(String)
-    PHONE_NUMBER = Column(String)
+    EMAIL = Column(String, unique= True)
+    PHONE_NUMBER = Column(String, unique= True)
     HOME_ADDRESS = Column(String)
+
 
 
 # Create tables
 Base.metadata.create_all(engine)
-
-
 
 
 class DocteurRegistration:
@@ -44,18 +41,8 @@ class DocteurRegistration:
     Class that handles docteur registration
     '''
     def __init__(self):
-        self.docteurs = {
-            "first_name": input("Enter first name: "),
-            "last_name": input("Enter last name: "),
-            "specialization": input("Enter specialization: "),
-            "license": input("Enter license: "),
-            "date_of_birth": input("Enter date of birth 'YY-MM-DD': "),
-            "sex": input("Enter M or F: "),
-            "email": input("Enter email: "),
-            "phone": input("Enter phone number: "),
-            "home_address": input("Enter home address: ")
-        }
         pass
+
 
     def registration(self):
 
@@ -66,18 +53,30 @@ class DocteurRegistration:
 
 
         nbr_doctors = int(input("Enter the number of doctors you want to register: "))
+        doctors = {
+            "first_name": input("Enter first name: "),
+            "last_name": input("Enter last name: "),
+            "specialization": input("Enter specialization: "),
+            "license": input("Enter license: "),
+            "date_of_birth": input("Enter date of birth 'YY-MM-DD': "),
+            "sex": input("Enter M or F: "),
+            "email": input("Enter email: "),
+            "phone": input("Enter phone number: "),
+            "home_address": input("Enter home address: ")
+        }
+
 
         for count in range(nbr_doctors):
             new_doctor = Doctor(
-                FIRST_NAME= self.docteurs["first_name"],
-                LAST_NAME = self.docteurs["last_name"],
-                SPECIALIZATION = self.docteurs["specialization"],
-                LICENSE = self.docteurs["license"],
-                DATE_OF_BIRTH = self.docteurs["date_of_birth"],
-                SEX = self.docteurs["sex"],
-                EMAIL = self.docteurs["email"],
-                PHONE_NUMBER = self.docteurs["phone"],
-                HOME_ADDRESS = self.docteurs["home_address"]
+                FIRST_NAME= doctors["first_name"],
+                LAST_NAME = doctors["last_name"],
+                SPECIALIZATION = doctors["specialization"],
+                LICENSE = doctors["license"],
+                DATE_OF_BIRTH = doctors["date_of_birth"],
+                SEX = doctors["sex"],
+                EMAIL = doctors["email"],
+                PHONE_NUMBER = doctors["phone"],
+                HOME_ADDRESS = doctors["home_address"]
             )
             session.add(new_doctor)
             session.commit()
@@ -89,7 +88,9 @@ class DocteurRegistration:
     def read_record(self):
         all_doctors = session.query(Doctor).all()
         doctor_list = [doctor.__dict__ for doctor in all_doctors]
-        print(doctor_list)
+        for i in doctor_list:
+            print(i.values())
+        #print(first_element)
 
 
     def update_record(self):
@@ -101,5 +102,4 @@ class DocteurRegistration:
 
 if __name__ == "__main__":
     doc = DocteurRegistration()
-    doc.registration()
     doc.read_record()
---
+++ try.py
@@ -0,0 +1,6 @@
+a_name = input("Enter a name").lower()
+if a_name == "bode":
+    print("Found!")
+    print(a_name)
+else:
+    print("Not found!")
