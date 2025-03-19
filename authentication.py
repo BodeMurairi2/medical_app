3db087dec9c84a3f9da67e1ca3f4803d046d5696
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-19 15:27:25 +0200

    Authentication start

---
+++ authentication.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import hashlib
+import getpass
+import argparse
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+# Create the database URL
+Database_url = "sqlite://login_details.db"
+engine = create_engine(Database_url)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Define the database ORM
+base = declarative_base()
+
+class LoginDetails(base):
+    __tablename__ = "Login Details"
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    hospital_name = Column(String)
+    hospital_email = Column(String, unique=True)
+    hospital_password = Column(String, unique=False)
+
+# Ensure the database table exists
+Base.metadata.create_all(engine)
+
+
+class Authentication:
+    def __init__(self):
+        pass
+    
+    def register_hospital(self):
+        hospital_details = {
+            "Hospital Name": input("Enter the hospital name").upper(),
+            "Hospital Email Address": input("Enter hospital email"),
+            "Hospital Password": input("Enter hospital password")
+        }
