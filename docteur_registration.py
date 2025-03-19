bad5a2d97dcca8fefd937dcce381794e8890b848
Faithirakoze <f.irakoze2@alustudent.com>
2025-03-19 14:55:12 -0700

    Creating the doctor's table

--- docteur_registration.py
+++ docteur_registration.py
@@ -9,3 +9,19 @@ DATABASE_URL = "sqlite:///doctors_data.db"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+# Define the ORM Model
+Base = declarative_base()
+
+class Doctor(Base):  # SQLAlchemy Model
+    __tablename__ = "Doctors"
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    FIRST_NAME = Column(String)
+    LAST_NAME = Column(String)
+    SPECIALIZATION = Column(String)
+    LICENSE = Column(String, unique=True)
+    DATE_OF_BIRTH = Column(String)
+    SEX = Column(String)
+    EMAIL = Column(String, unique=False)
+    PHONE_NUMBER = Column(String, unique=True)
+    HOME_ADDRESS = Column(String)
