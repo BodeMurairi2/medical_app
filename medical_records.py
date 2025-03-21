cde846b7a00ed22827ca99c6a11897655fe5d7ab
p-louis17 <p.nsigo@alustudent.com>
2025-03-21 13:08:15 +0200

    imports

---
+++ medical_records.py
@@ -0,0 +1,13 @@
+#Imports 
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+
+
+
+
+    
+
+
