711b2e6ec170b11b7f51714d990de805a396e558
Divine-kuzo <d.kuzo@alustudent.com>
2025-03-21 15:13:48 +0200

    new

--- medical_records.py
+++ medical_records.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import DateTime
 from datetime import datetime, timezone
 
 #create your database
