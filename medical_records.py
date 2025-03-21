13ff5195165809a94a29ff9146c4499321babc86
p-louis17 <p.nsigo@alustudent.com>
2025-03-21 15:11:11 +0200

    Fixed time

--- medical_records.py
+++ medical_records.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.exc import IntegrityError
-from datetime import datetime
+from sqlalchemy import DateTime
+from datetime import datetime, timezone
 
 #create your database
 engine = create_engine("sqlite:///medicalrecords.db",echo=False)
@@ -13,13 +14,13 @@ session = Session()
 
 #Define models
 class MedicalTable(Base):
-    _tablename_="medical_record"
+    __tablename__="medical_record"
     record_id = Column(Integer,primary_key=True, nullable=False)
     patient_id = Column (Integer, ForeignKey=True, nullable=False)
     diagnosis = Column(String)
     prescription = Column (String)
     doctor_name = Column(String, nullable=False)
-    date = Column(datetime)
+    date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 
 
