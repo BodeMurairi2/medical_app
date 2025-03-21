a8234aa9c321f75c1acde811dbf16aeb4d911b5b
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-21 11:55:35 +0200

    Authentification module

--- authentication.py
+++ authentication.py
@@ -2,9 +2,10 @@
 import hashlib
 import getpass
 import argparse
-from sqlalchemy import create_engine, Column, String, Integer
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 # Create the database URL
 Database_url = "sqlite://login_details.db"
