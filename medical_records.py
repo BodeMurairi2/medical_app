c4f25f2a55f5f49a54b9d80e0f8bf9dd39bf0ca4
Divine-kuzo <d.kuzo@alustudent.com>
2025-03-21 17:51:10 +0200

    Add data to the table

--- medical_records.py
+++ medical_records.py
@@ -15,12 +15,48 @@ session = Session()
 #Define models
 class MedicalTable(Base):
     __tablename__="medical_record"
-    record_id = Column(Integer,primary_key=True, nullable=False)
-    patient_id = Column (Integer, ForeignKey=True, nullable=False)
+    record_id = Column(Integer,primary_key=True, autoincrement=True, nullable=False)
+    patient_id = Column (Integer, ForeignKey=("patient_table.id"), nullable=False)
     diagnosis = Column(String)
     prescription = Column (String)
     doctor_name = Column(String, nullable=False)
-    date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    date = Column(DateTime, default=lambda: datetime.now(timezone.utc)) 
+
+# create the database
+#Base.metadata.create_all(engine)
+
+#Add data to the table
+
+def add_medical_data():
+    session = Session()
+    
+    patient_id = int(input("Enter patient ID:"))
+    diagnosis = input("Enter Diagnosis: ")
+    prescription = input("Enter prescription:")
+    doctor_name = input("Enter Doctor's Name:")
+
+    new_record = MedicalTable(
+        patient_id=patient_id,
+        diagnosis=diagnosis,
+        prescription=prescription,
+        doctor_name=doctor_name
+        )
+
+    # Add and commit the new record
+    session.add(new_record)
+    session.commit()
+    print("\n Medical record added successfully!")
+
+if __name__ == "__main__":
+    add_medical_data()
+
+
+
+
+    
+
+
+
 
 
 
