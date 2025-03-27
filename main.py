62fcf08b5891946c30db0a93f7f23f1613b308fe
N-Maurice <mauricenshimyumu@alustudent.com>
2025-03-27 13:48:20 +0200

    Hospital.py , updated:00_1

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python 3
-from medical_records import add_medical_records, read_medical_record
-from Patient_registration import PatientRegistration
+from hospital import PatientRegistration, MedicalRecordManagement
 from doctor_registration import DoctorRegistration, UpdateRecord
 
 
 def main():
     while True:
+        patient_registration = PatientRegistration()
+        medical_record_management = MedicalRecordManagement()
+        doctor_registration = DoctorRegistration()
+
 
         print("Welcome to the Hospital Database")
         print("1. Register a new patient")
@@ -15,8 +18,29 @@ def main():
         print("5. Update a doctor's information")
         print("6. Add a medical record")
         print("7. Read a medical record")
-        
-        choice = input("Enter the option: ")
+        print("8. Exit")
+
+          choice = int(input("Enter the option: "))
+        if choice == 1 :
+            patient_registration.register_patient()
+        elif choice == 2:
+            patient_registration.read_patient()
+        elif choice == 3:
+            doctor_registration.registration()
+        elif choice == 4:
+            doctor_registration.read_data()
+        elif choice == 5:
+            doctor_registration.read_data()
+        elif choice == 6:
+            medical_record_management.add_medical_record()
+        elif choice == 7:
+            medical_record_management.read_medical_record()
+        elif choice == 8:
+            break
+        else:
+            print("Error! Choose between 1-8")
+            continue
+
     
 
 if __name__ == "__main__":
